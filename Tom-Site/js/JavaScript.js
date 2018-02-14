01c14e4445e9dea5837d5dd98d57d6386ece0261
Tomm9005 <35025078+Tomm9005@users.noreply.github.com>
2018-02-14 13:47:30 +0200

    Update JavaScript.js

--- Tom-Site/js/JavaScript.js
+++ Tom-Site/js/JavaScript.js
@@ -1,4 +1,30 @@
-﻿// הגדרת מיקומים של נקודות עניין במרכז רפואי אסף הרופא
+    $(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+// הגדרת מיקומים של נקודות עניין במרכז רפואי אסף הרופא
 var Asaf = new google.maps.LatLng(31.966056, 34.839477); // מיקום כללי של אזור של המרכז הרפואי אסף הרופא 
 var myCenter = new google.maps.LatLng(31.966508, 34.840484); // נקודה מרכזית במפה של אסף הרופא 
 var myCenter2 = new google.maps.LatLng(31.967981, 34.842667); // קניון אסף הרופא
